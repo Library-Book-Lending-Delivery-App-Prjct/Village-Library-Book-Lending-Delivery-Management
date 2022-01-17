820b423cfd464b2a64df3ca36c9e8cbe012a15f4
Brahmaduttan S <brahmaduttan007@gmail.com>
2022-01-17 17:20:52 +0530

    First Commit

---
+++ app/src/main/java/com/example/booklendingdeliveryapp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.booklendingdeliveryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button getStartedBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getStartedBtn = (Button) findViewById(R.id.GetStartedBtn);
+
+
+        //implementing OnClickListener
+        getStartedBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
