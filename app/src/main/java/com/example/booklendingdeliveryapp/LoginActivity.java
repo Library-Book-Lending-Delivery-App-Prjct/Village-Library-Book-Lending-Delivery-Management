0196d8f3d71849a09ee8d8f76c9f119243fa90bc
Brahmaduttan S <brahmaduttan007@gmail.com>
2022-01-18 21:19:39 +0530

    Firebase Connection

--- app/src/main/java/com/example/booklendingdeliveryapp/LoginActivity.java
+++ app/src/main/java/com/example/booklendingdeliveryapp/LoginActivity.java
@@ -13,7 +13,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         }
         setContentView(R.layout.activity_login);
