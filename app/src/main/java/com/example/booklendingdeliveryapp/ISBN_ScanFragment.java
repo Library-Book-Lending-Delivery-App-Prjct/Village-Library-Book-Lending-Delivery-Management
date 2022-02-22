1031f4476c4e1098ec4f6455a0100772359d77dc
christy951 <christyrajan99@gmail.com>
2022-02-22 16:12:16 +0530

    ISBN
    Scanner Updated

--- app/src/main/java/com/example/booklendingdeliveryapp/ISBN_ScanFragment.java
+++ app/src/main/java/com/example/booklendingdeliveryapp/ISBN_ScanFragment.java
@@ -1,64 +1,38 @@
 package com.example.booklendingdeliveryapp;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ISBN_ScanFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ISBN_ScanFragment extends Fragment {
+import androidx.fragment.app.Fragment;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import com.google.zxing.Result;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
-    public ISBN_ScanFragment() {
-        // Required empty public constructor
-    }
+public class ISBN_ScanFragment extends Fragment implements ZXingScannerView.ResultHandler{
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ISBN_ScanFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ISBN_ScanFragment newInstance(String param1, String param2) {
-        ISBN_ScanFragment fragment = new ISBN_ScanFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private ZXingScannerView scannerView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_i_s_b_n__scan, container, false);
+        View view = inflater.inflate(R.layout.fragment_i_s_b_n__scan, container, false);
+
+        scannerView = view.findViewById(R.id.scannerView);
+        scannerView.setAspectTolerance(0.5f);
+        scannerView.setResultHandler(ISBN_ScanFragment.this);
+        scannerView.startCamera();
+
+        return view;
     }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        Toast.makeText(getActivity(),rawResult.getText(),Toast.LENGTH_LONG).show();
+    }
+
 }
