7bed39f9d9d90198ed3297a28d2579ab2b585756
Brahmaduttan S <brahmaduttan007@gmail.com>
2022-03-03 09:58:40 +0530

    updation

--- app/src/main/java/com/example/booklendingdeliveryapp/RecyclerViewItemDisplay.java
+++ app/src/main/java/com/example/booklendingdeliveryapp/RecyclerViewItemDisplay.java
@@ -1,18 +1,34 @@
 package com.example.booklendingdeliveryapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RecyclerViewItemDisplay extends AppCompatActivity {
 
     TextView mtitle,mauthor,mpublisher,misbn;
-    ImageView mimage;
+    ImageView mimage,Favourite;
+    Button addCart,bookNow;
+
+    boolean isInMyFavourite = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +40,9 @@ public class RecyclerViewItemDisplay extends AppCompatActivity {
         mpublisher = findViewById(R.id.rPublisherView2);
         misbn = findViewById(R.id.rISBNView2);
         mimage = findViewById(R.id.rImageView2);
+        addCart = findViewById(R.id.addCartBtn);
+        bookNow = findViewById(R.id.bookNowBtn);
+        Favourite = findViewById(R.id.FavouriteBtn);
 
         byte[] bytes = getIntent().getByteArrayExtra("image");
         String title = getIntent().getStringExtra("title");
@@ -39,6 +58,71 @@ public class RecyclerViewItemDisplay extends AppCompatActivity {
         misbn.setText(isbn);
         mimage.setImageBitmap(bmp);
 
+        Favourite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Favourite.setImageResource(R.drawable.ic_favorite);
+                Toast.makeText(RecyclerViewItemDisplay.this, "Book added to Favourites", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+    public static void addToFavorite(Context context, String bookId){
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() == null){
+            Toast.makeText(context,"You're not logged in",Toast.LENGTH_SHORT).show();
+        }
+        else{
+            long timestamp = System.currentTimeMillis();
+
+            HashMap<String, Object> hashMap = new HashMap<>();
+            hashMap.put("bookId", ""+bookId);
+            hashMap.put("timestamp", ""+timestamp);
+
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+            reference.child(firebaseAuth.getUid()).child("favourites").child(bookId)
+                    .setValue(hashMap)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            Toast.makeText(context,"Book Added to your favourites",Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(context,"Failed to Add book to your favourites due to"+e.getMessage(),Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+        }
+    }
+
+    public static void removeFromFavourite(Context context, String bookId){
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() == null){
+            Toast.makeText(context,"You're not logged in",Toast.LENGTH_SHORT).show();
+        }
+        else{
+
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+            reference.child(firebaseAuth.getUid()).child("favourites").child(bookId)
+                    .removeValue()
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            Toast.makeText(context,"Book removed from your favourites",Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(context,"Failed to remove book from your favourites due to"+e.getMessage(),Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+        }
     }
 
     @Override
